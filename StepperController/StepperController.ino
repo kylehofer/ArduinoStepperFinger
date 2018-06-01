ac0fcb79f7e4292ff518b1a25f1707d8357b7dbb
Kyle Hofer <KyleHofer@Neurak.com.au>
2018-06-01 21:48:48 +1000

    Updated Communication
    Changed to Serial from I2C to remove bugs related to I2C interrupts and timers.
    Fixed Timers.

--- StepperController/StepperController.ino
+++ StepperController/StepperController.ino
@@ -1,5 +1,4 @@
 #include <inttypes.h>
-#include <Wire.h>
 
 #define M1_STEP_PIN 9
 #define M1_DIRECTION_PIN 8
@@ -15,13 +14,15 @@
 
 #define TIME_OUT 500
 
-#define BASE_COMPARE_REGISTER 62	//Fastest Step Speed
+#define BASE_COMPARE_REGISTER 64	//Fastest Step Speed
 
 #define STEP_FLAG_BIT _BV(5)		
 #define DIRECTION_FLAG_BIT _BV(4)	
 #define FEED_FLAG_BITS 0b1111
 #define DATA_TYPE_FLAG _BV(7)
 #define MOTOR_FLAG _BV(6)
+#define START_OF_MESSAGE 1
+#define END_OF_MESSAGE _BV(1)
 
 class Stepper {
 
@@ -161,11 +162,8 @@ void setup()
 
 	_feedRate = BASE_COMPARE_REGISTER;
 
-	Wire.begin(0x8);// join i2c bus with address #8
-	Wire.onReceive(recieveData); // register event
-	Wire.onRequest(masterRequest); // register event
-	Serial.begin(115200);
-	//Serial1.begin(115200);
+	pinMode(0, INPUT);           // set pin to input
+	digitalWrite(0, HIGH);       // turn on pullup resistors
 
 	pinMode(M1_STEP_PIN, OUTPUT);
 	pinMode(M2_STEP_PIN, OUTPUT);
@@ -180,12 +178,22 @@ void setup()
 	digitalWrite(MS2_PIN, HIGH);
 	digitalWrite(MS3_PIN, LOW);
 
+	Serial.begin(115200);
+	Serial1.begin(115200);
+
 	_middle = new Stepper(M1_STEP_PIN, M1_DIRECTION_PIN);
 	_proximal = new Stepper(M2_STEP_PIN, M2_DIRECTION_PIN);
 
 	timerSetup();
-
 }
+
+/////////////////////////////////////////////////
+//												/
+//												/
+//				STEP FUNCTIONS					/
+//												/
+//												/
+/////////////////////////////////////////////////
 
 void updateFeed() {
 	uint8_t mFeed = _middle->getFeed(), pFeed = _proximal->getFeed();
@@ -194,15 +202,18 @@ void updateFeed() {
 	_hasProximal = true;
 
 	if (mFeed < pFeed) {
-		OCR1B =  _feedRate;
-		_OCR1BMax = OCR1B;
-		OCR1A = (_feedRate * mFeed) / pFeed;
+		//_OCR1BMax = _feedRate;
+
+		OCR1A = (_feedRate * pFeed) / mFeed;
+		//OCR1B = _OCR1BMax;
+				
 		timer1COMPB = &middleStep;
 		timer1COMPA = &proximalStep;
 	} else {
-		OCR1B = _feedRate;
-		_OCR1BMax = OCR1B;
-		OCR1A  = (_feedRate  * pFeed) / mFeed;
+		//_OCR1BMax = _feedRate;	
+		OCR1A = (_feedRate  * mFeed) / pFeed;
+		//OCR1B = _OCR1BMax;
+			
 		timer1COMPB = &proximalStep;
 		timer1COMPA = &middleStep;
 	}
@@ -217,7 +228,7 @@ bool nextCommand() {
 }
 
 bool middleStep() {
-	if (!_middle->step()) {
+	if (!_hasMiddle || !_middle->step()) {
 		if (!_hasProximal) {
 			return nextCommand();
 		}
@@ -228,7 +239,8 @@ bool middleStep() {
 }
 
 bool proximalStep() {
-	if (!_proximal->step()) {
+	
+	if (!_hasProximal || !_proximal->step()) {
 		if (!_hasMiddle) {
 			return nextCommand();
 		}
@@ -238,14 +250,24 @@ bool proximalStep() {
 	return true;
 }
 
+/////////////////////////////////////////////////
+//												/
+//												/
+//				TIMER FUNCTIONS					/
+//												/
+//												/
+/////////////////////////////////////////////////
+
 ISR(TIMER1_COMPB_vect) {
-	//OCR1B += _OCR1BMax;
+	OCR1B += _OCR1BMax;
 	if (!(*timer1COMPB)())
 		timerStop();
 }
 
 ISR(TIMER1_COMPA_vect) {
-	//OCR1B -= OCR1A;
+	OCR1B -= OCR1A;
+	//if (OCR1B > OCR1A)
+	//	OCR1B = _OCR1BMax;
 	if (!(*timer1COMPA)())
 		timerStop();
 }
@@ -254,34 +276,29 @@ void timerSetup() {
 	noInterrupts();						//Disable interrupts
 	TCCR1A = 0;
 	TCCR1B = 0;
-
 	TCCR1B = _BV(WGM12) | _BV(CS12);	//CTC Mode with a 256 prescaler	
 
+	_OCR1BMax = _feedRate;
+	OCR1B = _feedRate;
+
+	//
 	interrupts();						//Enable interrupts 	
 }
 
 void timerStart() {
-
-	Serial.println("Starting Timer!");
-
 	_isTimer = true;
 	noInterrupts();						//Disable interrupts
-
+	updateFeed();
 	TCNT1 = 0;
 
-	TIMSK1 |= _BV(OCIE1A) | _BV(OCIE1B);				//Output compare register A enabled
+	TIFR1 |= _BV(OCF1A) | _BV(OCF1B);	//Clearing interrupt flags
 
-	//updateMax();
-
-	//OCR1A = min(_m1Max, _m2Max);		//Output compare register value
+	TIMSK1 |= _BV(OCIE1A) | _BV(OCIE1B);//Output compare register A enabled
 
 	interrupts();						//Enable interrupts
 }
 
 void timerStop() {
-
-	Serial.println("Stopping Timer!");
-
 	_isTimer = false;
 	noInterrupts();						//Disable interrupts
 	TIMSK1 &= ~_BV(OCIE1A);				//Output compare register A Disable
@@ -289,67 +306,77 @@ void timerStop() {
 	interrupts();						//Enable interrupts
 }
 
-void masterRequest() {
-	Serial.println("Data Available!");
-
-	if (!_isTransmission)
-		Wire.write(true);
-	else
-		_dataRequested = true;
-
-	
-	_timeOutCount = 0;
-
-	//if (Wire.available() <= 0)
-	//	while (Wire.available() <= 0 && _timeOutCount < TIME_OUT) { _timeOutCount++; delay(1); }	
-}
+/////////////////////////////////////////////////
+//												/
+//												/
+//			Communication Functions				/
+//												/
+//												/
+/////////////////////////////////////////////////
 
 void recieveCommands(uint8_t flags, uint8_t count) {
 	Serial.print("Command Flags:");
 	Serial.println(flags,2);
 	Serial.print("Command Count:");
-	Serial.println(count,2);
+	Serial.println(count);
 	if ((flags & MOTOR_FLAG) > 0) {
 		_middle->addCommand(flags, count);
 	} else {
 		_proximal->addCommand(flags, count);
 	}
 }
-void recieveData(int size) {
-	_isTransmission = true;
+void recieveFeed(uint8_t flags, uint8_t feedL, uint8_t feedH) {
+
+}
+
+void serialRecieve() {
+
 	bool commandRecieved = false;
 
-	while (Wire.available() > 0) {
-		uint8_t data = Wire.read();
+	while (Serial1.available() > 0) {
+		uint8_t data = Serial1.read();
 
-		//Serial.print("Data:");
-		//Serial.println(data,2);
+		Serial.print("Data:");
+		Serial.println(data,2);
 
-		if ((data ^ DATA_TYPE_FLAG) > 0) {
-			recieveCommands(data, Wire.read());
-			commandRecieved = true;
+		if (_isTransmission) {
+			if ((data & DATA_TYPE_FLAG) > 0) {
+				recieveCommands(data, Serial1.read());
+				commandRecieved = true;
+			}
+			else if (data == END_OF_MESSAGE) {
+				Serial.println("END OF MESSAGE");
+				if (commandRecieved & !_isTimer) {
+					timerStart();
+				}
+				_isTransmission = false;
+			}
 		}
-		else {
-
+		else if (data == START_OF_MESSAGE) {
+			Serial.println("START OF MESSAGE");
+			_isTransmission = true;
 		}
-	}
 
-	if (_dataRequested)
-		Wire.write(true);
-
-	if (commandRecieved & !_isTimer) {
-		updateFeed();
-		timerStart();
 	}
+	
 	_isTransmission = false;
 }
-
-	//if (_dataRequested)
-	//	Wire.write(true);
-
-	_isTransmission = false;
-}
-
 
 void loop() {
+	if (Serial1.available() > 0) {
+		serialRecieve();
+	}
+	/*
+	if (_bOCR1A != OCR1A) {
+		Serial.print("OCR1A: ");
+		Serial.println(OCR1A);
+		_bOCR1A = OCR1A;
+	}
+
+	if (_bOCR1B != OCR1B) {
+		Serial.print("OCR1B: ");
+		Serial.println(OCR1B);
+		_bOCR1B = OCR1B;
+	}
+	*/
 }
